59116ffc1ff4d03f95dda9bbc827840219abe7d4
Sara Cashman <saramcashman@gmail.com>
2016-07-19 23:00:46 -0400

    updated events

--- js/app.js
+++ js/app.js
@@ -1,9 +1,44 @@
+
+function playHadouken () {
+  	$('#hadouken-sound')[0].volume = 0.5;
+  	$('#hadouken-sound')[0].load();
+  	$('#hadouken-sound')[0].play();
+}
+
 $(document).ready(function(){
 
 $('.ryu').mouseenter(function() {
-    alert('mouse entered .ryu div');
-  });
+    $('.ryu-still').hide();
+	$('.ryu-ready').show();
+    })
 
+	.mouseleave(function() {
+	$(".ryu-still").show();
+	$(".ryu-ready").hide();
 
+ 	})
+
+	.mousedown(function() {
+	playHadouken();
+	$(".ryu-still, .ryu-ready").hide();
+	$(".ryu-throwing").show();
+	$(".hadouken").show().animate({"left": "1020px" }, 500, function( ) {
+	$(this).hide();
+	$(this).css("left", "618px");
+	});
+	})
+
+	.mouseup(function() {
+	$(".ryu-throwing").hide();
+	$(".hadouken").hide();
+	$(".ryu-ready").show();
+	});
+
+	$(document).keydown(function(e) {
+  	if (e.which==88){
+  		$(".ryu-still, .ryu-ready").hide();
+  		$(".ryu-cool").show();
+  	}
+	});
 
 });
